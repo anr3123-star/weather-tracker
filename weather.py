442ca89436d3735f6c83db0def4a206a047431ca
anr3123-star <anr3123@gmail.com>
2026-07-16 14:50:34 -0700

    Log temperature in both Celsius and Fahrenheit
    Added Fahrenheit temperature calculation and logging.

--- weather.py
+++ weather.py
@@ -55,10 +55,13 @@ def get_current_weather(lat, lon):
 current_data = get_current_weather(LATITUDE,LONGITUDE)   
 current_temp = current_data["current"]["temperature_2m"]
 current_time = current_data["current"]["time"]
+temp_c = current_temp
+temp_f = round(temp_c * 9/5 + 32, 1)
 log_df = pd.DataFrame({
     "date": [str(today)],
     "time": [current_time],
-    "temperature_2m": [current_temp]
+    "temperature_2m": [current_temp],
+    "temp_f": [temp_f]
 })
 log_file = "daily_log.csv"
 
